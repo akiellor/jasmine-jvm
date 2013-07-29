c718b1dd0ca66dbe99589cd6fe7ae2eb989c1ce4
Andrew Kiellor <akiellor@gmail.com>
2013-07-29 09:09:39 -0700

    Argument parsing for SpecScanner.

--- src/main/java/org/jasmine/cli/Main.java
+++ src/main/java/org/jasmine/cli/Main.java
@@ -5,16 +5,50 @@ import com.google.common.collect.Multimap;
 import org.jasmine.Failure;
 import org.jasmine.Identifier;
 import org.jasmine.Notifier;
+import org.jasmine.Runtime;
+import org.jasmine.Runtime;
+import org.jasmine.SpecScanner;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 public class Main {
+    public static class Arguments{
+        @Option(name = "--pattern")
+        private String pattern;
+
+        @Argument
+        private List<String> arguments = new ArrayList<String>();
+
+        public Iterable<String> specs(){
+            if(pattern != null){
+                return new SpecScanner().findSpecs(pattern);
+            }else{
+                return arguments;
+            }
+        }
+    }
+
     public static void main(String... args){
-        new org.jasmine.Runtime(Arrays.asList(args)).execute(new Notifier() {
+        Arguments arguments = new Arguments();
+        CmdLineParser parser = new CmdLineParser(arguments);
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            throw new RuntimeException(e);
+        }
+
+        Runtime runtime = new Runtime(arguments.specs());
+
+        runtime.execute(new Notifier() {
             private Multimap<Identifier, Failure> failures = HashMultimap.create();
             private Map<Identifier, String> descriptions = new HashMap<>();
 
@@ -39,17 +73,17 @@ public class Main {
             public void finished() {
                 System.out.println();
                 System.out.println();
-                for(Map.Entry<Identifier, Collection<Failure>> entry : failures.asMap().entrySet()){
+                for (Map.Entry<Identifier, Collection<Failure>> entry : failures.asMap().entrySet()) {
                     System.out.println(descriptions.get(entry.getKey()));
                     System.out.println();
-                    for(Failure failure : entry.getValue()){
+                    for (Failure failure : entry.getValue()) {
                         System.out.println(failure.getStackString().replaceAll("^", "  ").replaceAll("\\n", "\n  "));
                     }
                 }
                 System.out.println(String.format("%s/%s passed.",
                         descriptions.size() - failures.keySet().size(), descriptions.size()));
 
-                if(failures.size() > 0){
+                if (failures.size() > 0) {
                     System.exit(1);
                 }
             }
