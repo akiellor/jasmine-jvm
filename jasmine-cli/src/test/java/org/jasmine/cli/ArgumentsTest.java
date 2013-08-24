75d1749b7f7cb4d005c4bd3d31e8c9122d5085c5
Andrew Kiellor <akiellor@gmail.com>
2013-08-23 17:05:54 -0700

    Add a bunch of tests to the argument parser.

--- jasmine-cli/src/test/java/org/jasmine/cli/ArgumentsTest.java
+++ jasmine-cli/src/test/java/org/jasmine/cli/ArgumentsTest.java
@@ -1,10 +1,71 @@
 package org.jasmine.cli;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.UUID;
 
 import static org.fest.assertions.Assertions.assertThat;
 
 public class ArgumentsTest {
+    public static class FileRule implements TestRule {
+        private final File tmpDir;
+
+        public FileRule(File root) {
+            tmpDir = new File(root, "." + UUID.randomUUID().toString());
+        }
+
+        @Override
+        public Statement apply(final Statement base, Description description) {
+            return new Statement() {
+                @Override
+                public void evaluate() throws Throwable {
+                    tmpDir.mkdirs();
+                    base.evaluate();
+                    Files.walkFileTree(tmpDir.toPath(), new SimpleFileVisitor<Path>() {
+                        @Override
+                        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                            Files.deleteIfExists(file);
+                            return FileVisitResult.CONTINUE;
+                        }
+
+                        @Override
+                        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                            Files.deleteIfExists(dir);
+                            return FileVisitResult.CONTINUE;
+                        }
+                    });
+                }
+            };
+        }
+
+        public File file(String name) {
+            File file = new File(tmpDir, name);
+            try {
+                if (!file.exists() && !file.createNewFile()) {
+                    throw new RuntimeException("Could not create file: " + file);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return file;
+        }
+
+        public String path(String name) {
+            return new File(tmpDir, name).getPath();
+        }
+    }
+
+    @Rule
+    public FileRule files = new FileRule(new File("."));
+
     @Test
     public void shouldDefaultCompileModeToJIT(){
         Arguments arguments = Arguments.parse();
@@ -22,9 +83,42 @@ public class ArgumentsTest {
     }
 
     @Test
-    public void shouldParsePattern(){
-        Arguments arguments = Arguments.parse("--pattern", "./src/test/jasmine-1.3.1/core/Base*.js");
+    public void shouldUsePatternToFindSpecs() throws IOException {
+        files.file("ASpec.js");
+        files.file("BSpec.js");
 
-        assertThat(arguments.specs()).containsOnly("./src/test/jasmine-1.3.1/core/BaseSpec.js");
+        Arguments arguments = Arguments.parse("--pattern", files.path("A*.js"));
+
+        assertThat(arguments.specs()).containsOnly(files.path("ASpec.js"));
+    }
+
+    @Test
+    public void shouldUsePatternToFindMultipleSpecs() throws IOException {
+        files.file("ASpec.js");
+        files.file("BSpec.js");
+
+        Arguments arguments = Arguments.parse("--pattern", files.path("*Spec.js"));
+
+        assertThat(arguments.specs()).containsOnly(files.path("ASpec.js"), files.path("BSpec.js"));
+    }
+
+    @Test
+    public void shouldLoadSpecsFromTrailingArguments() throws IOException {
+        files.file("ASpec.js");
+        files.file("BSpec.js");
+
+        Arguments arguments = Arguments.parse(files.path("ASpec.js"), files.path("BSpec.js"));
+
+        assertThat(arguments.specs()).containsOnly(files.path("ASpec.js"), files.path("BSpec.js"));
+    }
+
+    @Test
+    public void shouldNotLoadSpecsFromTrailingArguments() throws IOException {
+        files.file("ASpec.js");
+        files.file("BSpec.js");
+
+        Arguments arguments = Arguments.parse(files.path("ASpec.js"));
+
+        assertThat(arguments.specs()).containsOnly(files.path("ASpec.js"));
     }
 }
